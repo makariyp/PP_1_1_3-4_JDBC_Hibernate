02ec48523c6e87be65c80936332d3738b556e4fe
Makariyp <makariyp@icloud.com>
2023-02-10 17:13:32 +0300

    Очистка таблицы, получение списка пользователей

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -4,8 +4,10 @@ import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
@@ -59,14 +61,54 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void removeUserById(long id) {
-
+        try (Connection connection = util.getConnection()) {
+            Statement statement = connection.createStatement();
+            String sql = "DELETE FROM USERS WHERE id = " + id + ";";
+            statement.executeUpdate(sql);
+            System.out.println("Пользователь " + id + " успешно удален.");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public List<User> getAllUsers() {
-        return null;
+        try (Connection connection = util.getConnection()) {
+            Statement statement = connection.createStatement();
+            String sql = "SELECT * FROM USERS;";
+            ResultSet resultSet = statement.executeQuery(sql);
+            List<User> result = new ArrayList<>();
+            while (resultSet.next()) {
+                Long id = resultSet.getLong("id");
+                String name = resultSet.getString("name");
+                String lastName = resultSet.getString("lastname");
+                Byte age = resultSet.getByte("age");
+
+                User user = new User(name, lastName, age);
+                user.setId(id);
+
+                result.add(user);
+            }
+            System.out.println("Пользователи успешно получены.");
+            return result;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void cleanUsersTable() {
-
+        try (Connection connection = util.getConnection()) {
+            Statement statement = connection.createStatement();
+            String sql = "TRUNCATE USERS;";
+            statement.executeUpdate(sql);
+            System.out.println("Таблица успешно очистилась.");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/model/User.java
+++ src/main/java/jm/task/core/jdbc/model/User.java
@@ -59,4 +59,6 @@ public class User {
     public void setAge(Byte age) {
         this.age = age;
     }
+
+
 }
